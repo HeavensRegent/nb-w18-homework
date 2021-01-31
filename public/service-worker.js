922272cd0a6cbd20d6c4d35fce81d73e2f400027
darthNater <blstarwars@live.com>
2021-01-30 20:35:49 -0700

    issues with old service worker running

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,10 +9,17 @@ const FILES_TO_CACHE = [
   "/db.js",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 // install
 self.addEventListener("install", function (evt) {
+  // pre cache image data
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
+    
   // pre cache all static assets
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
